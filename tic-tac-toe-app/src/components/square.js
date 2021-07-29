fe187a86bf2df107395cf17b0c78c57641310847
vijayanandof <vijayanandof@users.noreply.github.com>
2021-07-29 08:49:44 +0530

    Updates

--- tic-tac-toe-app/src/components/square.js
+++
@@ -1,9 +0,0 @@
-function Square(props) {
-  return (
-    <div className="squarebox" onClick={() => props.onClick(props.index)}>
-      { typeof props.state === 'number' ? "" : props.state}
-    </div>
-  );
-}
-
-export default Square;
